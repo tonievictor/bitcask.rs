b2c5bb028859978e7e41137160cba9463304319a
Tonie Victor <victornwanochi@gmail.com>
2025-01-25 07:02:54 +0100

    switched the writer to a more efficient bufwriter instead of just a file and setup the primary methods

--- bitcask/src/lib.rs
+++ bitcask/src/lib.rs
@@ -1,12 +1,13 @@
 use anyhow::Result;
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
+use std::io::BufWriter;
 use std::path::{Path, PathBuf};
 use ulid::Ulid;
 
 pub struct Bitcask {
     path: PathBuf,
-    writer: File,
+    writer: BufWriter<File>,
     // the value will be a json serializable string
     reader: HashMap<String, String>,
 }
@@ -17,11 +18,15 @@ impl Bitcask {
         let filename = Ulid::new().to_string();
         let path = dir.join(Path::new(&filename));
         let file = OpenOptions::new().create(true).append(true).open(&path)?;
-
+        let bw = BufWriter::new(file);
         Ok(Bitcask {
             path,
-            writer: file,
+            writer: bw,
             reader: HashMap::new(),
         })
     }
+
+    pub fn set(&mut self, key: String, value: String) -> Result<()> {}
+    pub fn get(&self, key: String) -> Option<String> {}
+    pub fn remove(&mut self, key: String) -> Result<()> {}
 }
