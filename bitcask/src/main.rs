c723f3052fafc9cad2c3eb6220f77f67459fb537
Tonie Victor <victornwanochi@gmail.com>
2025-01-26 14:06:16 +0100

    reimplemented the cli, this time using a process(loop). This enables the program to use a shared memory for multiple commands in the same process. Previous impementation was not proper as a new keydir was created on each command (don't know why I even did that to be honest). Also unique log files are created for each process.

--- bitcask/src/main.rs
+++ bitcask/src/main.rs
@@ -1,63 +1,100 @@
 use bitcask::Bitcask;
-use clap::Parser;
+use std::io::stdin;
 use std::path::Path;
 use std::process::exit;
+use ulid::Ulid;
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// option - set | get | remove
-    option: String,
+#[derive(Debug)]
+enum Action {
+    Set,
+    Get,
+    Remove,
+}
 
-    /// key
-    #[arg(short, long)]
+#[derive(Debug)]
+struct Command {
+    action: Action,
     key: String,
-
-    /// value
-    #[arg(short, long, default_value_t = String::from(""))]
     value: String,
 }
 
 fn main() {
-    let args = Args::parse();
     let mut store = init_store("trial");
-    match args.option.as_str() {
-        "set" => match store.set(args.key.as_str(), args.value.as_str()) {
-            Ok(_) => {
-                println!("value is set");
-                exit(0);
-            }
-            Err(err) => {
-                eprintln!("Cannot set key: {:?}", err);
-                exit(1);
-            }
-        },
-        "get" => match store.get(args.key) {
-            Ok(res) => match res {
-                Some(val) => {
-                    println!("{}", val);
-                    exit(0)
-                }
-                None => {
-                    eprintln!("Key does not exist");
-                    exit(1);
-                }
+    loop {
+        let mut input = String::new();
+        stdin()
+            .read_line(&mut input)
+            .expect("Failed to read user input");
+
+        match parse_input(input) {
+            Ok(command) => match command.action {
+                Action::Set => match store.set(command.key.as_str(), command.value.as_str()) {
+                    Ok(_) => println!("Successfully set key value pair"),
+                    Err(err) => eprintln!("An error occured: {}", err),
+                },
+                Action::Get => match store.get(command.key) {
+                    Ok(obj) => match obj {
+                        Some(v) => println!("{}", v),
+                        None => println!("Key does not exist in the database"),
+                    },
+                    Err(err) => eprintln!("An error occured: {}", err),
+                },
+
+                Action::Remove => unimplemented!(""),
             },
-            Err(err) => {
-                eprintln!("An error occurred while getting the value {:?}", err);
-                exit(1);
-            }
-        },
-        _ => {
-            println!("hello, who goes");
-            exit(1);
+            Err(_) => continue,
         }
     }
 }
 
+fn parse_input(mut input: String) -> Result<Command, ()> {
+    input.pop();
+    let newinput: Vec<_> = input.split(" ").collect();
+
+    let mut value = String::from("");
+
+    let action = match newinput[0] {
+        "set" => {
+            if newinput.len() != 3 {
+                eprintln!("Set command: set <key> <value>");
+                return Err(());
+            }
+            value = String::from(newinput[2]);
+            Action::Set
+        }
+        "get" => {
+            if newinput.len() != 2 {
+                eprintln!("Get command: get <key>");
+                return Err(());
+            }
+            Action::Get
+        }
+        "remove" => {
+            if newinput.len() != 2 {
+                eprintln!("Remove command: remove <key>");
+                return Err(());
+            }
+            Action::Remove
+        }
+        "exit" => {
+            exit(0);
+        }
+        _ => {
+            println!("invalid command");
+            return Err(());
+        }
+    };
+
+    Ok(Command {
+        action,
+        key: String::from(newinput[1]),
+        value,
+    })
+}
+
 fn init_store(dir_name: &str) -> Bitcask {
     let dir = Path::new(dir_name);
-    let filename = String::from("log.btk");
+    let filename = Ulid::new().to_string();
     let path = dir.join(Path::new(&filename));
 
     match Bitcask::open(path) {
